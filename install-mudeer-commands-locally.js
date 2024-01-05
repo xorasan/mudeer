f56278929ae4c78c9d138246251b6f4bccc4c8e2
Hasan Xorasani <husnainlatif@gmail.com>
2024-01-05 23:37:12 +0500

    changed install path to ~/bin, apparently ~/.local/bin isn't standard enough -_-

--- install-mudeer-commands-locally.js
+++ install-mudeer-commands-locally.js
@@ -3,7 +3,7 @@ var path = require('path');
 var fs = require('fs');
 var log = console.log;
 
-log(' This script creates shell scripts in ~/.local/bin ');
+log(' This script creates shell scripts in ~/bin ');
 log(' They point to mudeer-*.js files in this directory ');
 log(' The path is static so run this script again ');
 log(' if you move mudeer elsewhere ');
@@ -11,7 +11,7 @@ log(' if you move mudeer elsewhere ');
 log('\n '+process.cwd()+'\n');
 
 try {
-	fs.mkdirSync(process.env.HOME+'/.local/bin');
+	fs.mkdirSync(process.env.HOME+'/bin');
 } catch (e) {
 }
 
@@ -23,9 +23,9 @@ function generate_sh_script(name) {
 fs.readdirSync('.').forEach(file => {
 	if (file.startsWith('mudeer-') && file.endsWith('.js')) {
 		var name = file.slice(0, -3);
-		log(' '+process.env.HOME+'/.local/bin/'+name);
+		log(' '+process.env.HOME+'/bin/'+name);
 		var full_path = process.cwd()+'/'+file;
-		var target_path = process.env.HOME+'/.local/bin/'+name;
+		var target_path = process.env.HOME+'/bin/'+name;
 		fs.writeFileSync(target_path, generate_sh_script(full_path));
 		fs.chmodSync(target_path, '755');
 //		fs.symlinkSync(process.env.HOME+'/.local/bin/'+name, process.cwd()+'/'+file);
@@ -33,6 +33,6 @@ fs.readdirSync('.').forEach(file => {
 });
 
 log(' in case these don\'t work, log out log back in again to the shell ');
-log(' or do: source ~/.profile \n');
+log(' or do: source ~/.profile (might not work) \n');
 
 // https://askubuntu.com/questions/1144231/home-local-bin-not-in-path-for-ubuntu-19-04
